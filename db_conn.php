e1b22909be3dde45e0717d10cd343b4d94fa97a1
Suresh <suresh.thagunna77@gmail.com>
2024-04-16 13:14:47 -0400

    create database connection class

---
+++ db_conn.php
@@ -0,0 +1,39 @@
+<?php
+
+class DatabaseConnection
+{
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+    const DB_NAME = 'antique_shoes';
+    const DB_HOST = 'localhost';
+
+    private $dbc;
+
+    function __construct()
+    {
+        $this->dbc = @mysqli_connect(
+            self::DB_HOST,
+            self::DB_USER,
+            self::DB_PASSWORD,
+            self::DB_NAME
+        )
+            or die(
+                'Could not connect to MySQL: ' . mysqli_connect_error()
+            );
+
+        mysqli_set_charset($this->dbc, 'utf8');
+    }
+
+    function prepare_string($string)
+    {
+        $string = strip_tags($string);
+        $string = mysqli_real_escape_string($this->dbc, trim($string));
+        return $string;
+    }
+
+    function get_dbc()
+    {
+        return $this->dbc;
+    }
+
+}
