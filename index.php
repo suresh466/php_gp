723941940ca21a28d6b6ea8f7da2eb2958148ca8
Suresh <suresh.thagunna77@gmail.com>
2024-04-16 13:25:16 -0400

    create index.php and add button to go to products

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antique Shoes</title>
+</head>
+<body>
+    <h1> Welcome to Antique Shoes</h1>
+    <button><a href="products.php">Start shopping</a></button>
+</body>
+</html>
