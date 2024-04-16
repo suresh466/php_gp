9ede6792008f874848158b67e29cce91e0f25fec
Suresh <suresh.thagunna77@gmail.com>
2024-04-16 13:20:26 -0400

    create shoes class

---
+++ Shoes.php
@@ -0,0 +1,92 @@
+<?php
+
+class Shoes {
+    private $db;
+
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+    
+    function add_shoe($shoe_name, $shoe_price, $shoe_size, $shoe_color, $shoe_brand)
+    {
+        $shoe_name_clean = $this->db->prepare_string($shoe_name);
+        $shoe_price_clean = $this->db->prepare_string($shoe_price);
+        $shoe_size_clean = $this->db->prepare_string($shoe_size);
+        $shoe_color_clean = $this->db->prepare_string($shoe_color);
+        $shoe_brand_clean = $this->db->prepare_string($shoe_brand);
+
+        $query = "INSERT INTO shoes(shoe_name, shoe_price, shoe_size, shoe_color, shoe_brand) VALUES (?,?,?,?,?)";
+
+        $stmt = mysqli_prepare($this->db->dbc, $query);
+
+        mysqli_stmt_bind_param(
+            $stmt,
+            'sssss',
+            $shoe_name_clean,
+            $shoe_price_clean,
+            $shoe_size_clean,
+            $shoe_color_clean,
+            $shoe_brand_clean,
+        );
+
+        $result = mysqli_stmt_execute($stmt);
+
+        return $result;
+    }
+    
+    function get_shoes() {
+        $query = 'SELECT * FROM shoes;';
+        $result = @mysqli_query($this->db->dbc,$query);
+        return $result;
+    }
+    
+    function get_shoe_by_id($shoe_id) {
+        $shoe_id_clean = $this->db->prepare_string($shoe_id);
+        $query = "SELECT * FROM shoes WHERE shoe_id = ?";
+        $stmt = mysqli_prepare($this->db->dbc, $query);
+        mysqli_stmt_bind_param($stmt, 's', $shoe_id_clean);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        return $result;
+    }
+    
+    function update_shoe_by_id($shoe_id, $shoe_name, $shoe_price, $shoe_size, $shoe_color, $shoe_brand){
+        
+        $shoe_id_clean = $this->db->prepare_string($shoe_id);
+        $shoe_name_clean = $this->db->prepare_string($shoe_name);
+        $shoe_price_clean = $this->db->prepare_string($shoe_price);
+        $shoe_size_clean = $this->db->prepare_string($shoe_size);
+        $shoe_color_clean = $this->db->prepare_string($shoe_color);
+        $shoe_brand_clean = $this->db->prepare_string($shoe_brand);
+
+        $query = "UPDATE shoes SET shoe_name = ?, shoe_price = ?, shoe_size = ?, shoe_color = ?, shoe_brand = ? WHERE  shoe_id = ?;";
+
+        $stmt = mysqli_prepare($this->db->dbc, $query);
+
+        mysqli_stmt_bind_param(
+            $stmt,
+            'ssssss',
+            $shoe_name_clean,
+            $shoe_price_clean,
+            $shoe_size_clean,
+            $shoe_color_clean,
+            $shoe_brand_clean,
+            $shoe_id_clean
+        );
+
+        $result = mysqli_stmt_execute($stmt);
+
+        return $result;
+    }
+
+    function delete_shoe_by_id($shoe_id) {
+        $shoe_id_clean = $this->db->prepare_string($shoe_id);
+        $query = "DELETE FROM shoes WHERE shoe_id = ?";
+        $stmt = mysqli_prepare($this->db->dbc, $query);
+        mysqli_stmt_bind_param($stmt, 's', $shoe_id_clean);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        return $result;
+    }
+}
